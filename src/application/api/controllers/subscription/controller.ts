e5b0ee915ee1d5b50552ba5db9693120a69513bd
Rustamxo'jayev Muhammadxo'ja <mukhammadkhodja.r@payme.uz>
2025-04-18 11:49:51 +0500

    removed unused imports

--- src/application/api/controllers/subscription/controller.ts
+++ src/application/api/controllers/subscription/controller.ts
@@ -1,10 +1,4 @@
-import { Body, Controller, Get, Post, Req, Res, UseGuards } from "@nestjs/common";
-import { CheckoutCreateLinkUseCase } from "src/application/usecases/checkout/create-link";
-import { AuthGuard } from "app/application/api/guard";
-import { decode, JwtPayload } from "jsonwebtoken";
-import { Request, Response } from "express";
-import { UserPlan } from "app/domain";
-import { LemonSqueezyProductId } from "app/domain/payment-processor/strategy/lemon-squeezy";
+import { Body, Controller, Get } from "@nestjs/common";
 
 @Controller("subscription")
 export class SubscrtiptionController {
