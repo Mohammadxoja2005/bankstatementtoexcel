1b15769997bf6f5f0f845029e9f9336d29449942
Rustamxojayev Muhammadxoja <muhammadxojaofficial@gmail.com>
2025-04-19 15:27:08 +0500

    added signature verification

--- src/application/api/controllers/subscription/controller.ts
+++ src/application/api/controllers/subscription/controller.ts
@@ -1,6 +1,8 @@
-import { Controller, Post, Headers, Req, Res } from "@nestjs/common";
+import { Controller, Post, Headers, Req, Res, RawBodyRequest } from "@nestjs/common";
 import { Request, Response } from "express";
 import { LemonSqueezyTransaction } from "app/domain";
+import * as crypto from "node:crypto";
+import * as process from "node:process";
 
 @Controller("subscription")
 export class SubscriptionController {
@@ -8,14 +10,31 @@ export class SubscriptionController {
 
     @Post("webhook/payment")
     async handlePaymentStatus(
-        @Req() request: Request,
+        @Req() request: RawBodyRequest<Request>,
         @Res() response: Response,
         @Headers("X-Signature") signature: string,
     ) {
         const transaction: LemonSqueezyTransaction = request.body;
 
-        console.log("payload webhook", transaction);
+        // console.log("payload webhook", transaction, signature);
 
+        const hmac = crypto.createHmac("sha256", process.env.LEMON_SQUEEZY_WEBHOOK_SIGNATURE || "");
+
+        const digest = Buffer.from(hmac.update(request.rawBody || "").digest("hex"), "utf8");
+        const expectedSignature = Buffer.from(signature, "utf8");
+
+        // const expectedSignature = crypto
+        //     .createHmac("sha256", "642a7fff-b8ba-4335-a886-0588f60043d9")
+        //     .update(JSON.stringify(request.body))
+        //     .digest("hex");
+
+        if (!crypto.timingSafeEqual(digest, expectedSignature)) {
+            throw new Error("Invalid signature.");
+        } else {
+            console.log("success");
+            console.log(request.rawBody);
+            console.log(request.body);
+        }
         // Perform your business logic based on payment status
         // if (paymentStatus === "paid") {
         //     // Update the user's checkout to active or grant access
--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,9 @@ import * as process from "node:process";
 
 async function bootstrap() {
     dotenv.config();
-    const app = await NestFactory.create(AppModule);
+    const app = await NestFactory.create(AppModule, {
+        rawBody: true,
+    });
 
     await app.listen(process.env.PORT ?? 3001);
 }
